8734326a9a6138a4a3a2af63dcbdb61374cba0d2
Connor Ashley <ashleycm@vcu.edu>
2021-06-26 11:36:14 -0400

    fixed bug in adding todos to list from the addTask method

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
+import { v4 as uuidv4 } from 'uuid';
+import './TodoList.css';
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: [{ task: 'Water Plant' }, { task: 'Add Nutrients to Plant' }]
+            todos: [{ task: 'Water Plant', id: uuidv4() }, { task: 'Add Nutrients to Plant', id: uuidv4() }]
         }
+        this.addTask = this.addTask.bind(this);
+    }
+    addTask(newTask) {
+        this.setState({
+            todos: [...this.state.todos, newTask]
+        })
     }
     render () {
         const todos = this.state.todos.map(todo => {
-            return <Todo task={todo.task} />
+            return <Todo 
+                    task={todo.task}
+                    key={todo.id} 
+                    />
         })
         return (
-            <div>
+            <div className="TodoList">
                 <h1>Todo List</h1>
+                <NewTodoForm 
+                    addTask={this.addTask}
+                />
                 <ul>
                     {todos}
                 </ul>
