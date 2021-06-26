95c7406e3b52e0bc0dae4c9da3e550d9345db869
Connor Ashley <ashleycm@vcu.edu>
2021-06-26 11:36:56 -0400

    corrected bug in handleSubmit

---
+++ src/NewTodoForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './NewTodoForm.css';
+
+class NewTodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            task: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.addTask(this.state);
+        this.setState({ task: '' });
+    }
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="task">Task: </label>
+                <input
+                    id="task"
+                    name="task"
+                    value={this.state.task}
+                    onChange={this.handleChange}
+                />
+                <button>Add to List</button>
+            </form>
+        )
+    }
+}
+
+export default NewTodoForm;
